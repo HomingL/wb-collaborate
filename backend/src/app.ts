d55289810169fe896e64bc3ba9ad283766f9dd1b
Hongming Li <homgming.li@mail.utoronto.ca>
2021-02-27 18:37:12 -0500

    Resolve_hello_resolver_path

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { HelloResolver } from './resolvers/hello.ts';
+import { HelloResolver } from './resolvers/hello';
 
 require('dotenv').config();
 
--- backend/src/resolvers/hello.ts
+++ backend/src/resolvers/hello.ts
@@ -4,7 +4,6 @@ import { Resolver, Query } from 'type-graphql';
 @Resolver()
 export class HelloResolver {
   @Query(() => String)
-  // eslint-disable-next-line class-methods-use-this
   hello() {
     return 'bye';
   }
