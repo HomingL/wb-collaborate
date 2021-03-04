1bca67e6d91e7511b611b52eb36e2c8cb834ee39
HensonChen <henson.99611@gmail.com>
2021-03-05 06:40:44 +0800

    index basic layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,27 +2,40 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
-import SigninForm from '../src/components/auth/SigninForm';
+// import SigninForm from '../src/components/auth/SigninForm';
+import SigninForm from '../pages/signin';
 import AuthFormLayout from '../src/components/auth/AuthFormLayout';
+import JoinBoard from '../src/components/JoinBoard'
+import { Grid } from '@material-ui/core';
 
 export default function Index() {
   return (
     <Container maxWidth="md">
       <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h5" component="h1" gutterBottom>
           WB Collaborate
         </Typography>
-        <Link href="/signup" color="secondary">
+        {/* <Link href="/signup" color="secondary">
           Go to the Sign Up page
-        </Link>
-        <AuthFormLayout title='Sign In' >
-          <SigninForm/>
-        </AuthFormLayout>
-        <ProTip />
-        <Copyright />
+        </Link> */}
+        <Grid container alignItems="center" justify="center">
+          <Grid item xs={7}>
+            <Typography variant="h4" component="h4">
+              Join existed board
+            </Typography>
+            <JoinBoard/>
+          </Grid>
+          <Grid item xs={5}>
+            <AuthFormLayout title='Sign In' >
+              <SigninForm/>
+            </AuthFormLayout>
+          </Grid>
+        </Grid>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
       </Box>
     </Container>
   );
