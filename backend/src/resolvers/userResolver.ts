a60151e612db879aefbb3ea6cb5994c5a2267236
Hongming Li <homgming.li@mail.utoronto.ca>
2021-03-06 18:38:09 -0500

    Eslint userResolver

--- backend/src/resolvers/userResolver.ts
+++ backend/src/resolvers/userResolver.ts
@@ -1,18 +1,18 @@
 import { Resolver, Query, Mutation, Arg, Ctx, UseMiddleware } from 'type-graphql';
 import { sign } from 'jsonwebtoken';
 import { Context } from 'src/context';
-import { User } from '../models/user';
 import crypto from 'crypto';
+import { User } from '../models/user';
 import { isAuthenticated } from '../middlewares/auth';
 
 const { TOKEN_SECRET, TOKEN_EXPIRE_TIME } = process.env;
 
-function generateSalt (){
+function generateSalt() {
   return crypto.randomBytes(16).toString('base64');
 }
 
-function generateHash (password: string, salt: string){
-  var hash = crypto.createHmac('sha512', salt);
+function generateHash(password: string, salt: string) {
+  const hash = crypto.createHmac('sha512', salt);
   hash.update(password);
   return hash.digest('base64');
 }
@@ -31,14 +31,14 @@ export class UserResolver {
     @Arg('email') email: string,
     @Arg('password') password: string,
   ) {
-    let user = await User.findOne({email: email});
-    if (user) return new Error("User Already exist");
+    const user = await User.findOne({ email });
+    if (user) return new Error('User Already exist');
     const salt = generateSalt();
+    // eslint-disable-next-line no-param-reassign
     password = generateHash(password, salt);
-    let us = await User.create({ name, email, salt, password }).save();
+    const us = await User.create({ name, email, salt, password }).save();
     return us;
   }
-
 
   @Mutation(() => User)
   async Signin(
@@ -51,7 +51,6 @@ export class UserResolver {
       const token = sign({ email: user.email, name: user.name, id: user.id }, TOKEN_SECRET!, {
         expiresIn: TOKEN_EXPIRE_TIME });
       const { res } = ctx;
-      // console.log("TOken scret is:", TOKEN_SECRET);
       res.cookie('token', token, {
         secure: true,
       });
