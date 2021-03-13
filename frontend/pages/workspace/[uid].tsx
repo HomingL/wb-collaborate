b5c1501e736021d1d192574c7c4c1ee19777555c
Hongming Li <homgming.li@mail.utoronto.ca>
2021-03-13 12:39:38 -0500

    gitflow-feature-stash: Whiteboard
    whiteboard sync test

--- frontend/pages/workspace/[uid].tsx
+++ frontend/pages/workspace/[uid].tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { GetUserHandler } from '../../src/graphql/handler/userHandler'
+import React, { useState } from 'react'
+import { Grid } from '@material-ui/core';
+import Whiteboard from '../../src/components/whiteboard/whiteboard'
 
 interface WorkspaceProps {
 
 }
 
-const Workspace: React.FC<WorkspaceProps> = ({}) => {
-    return <GetUserHandler/>;
+const Workspace: React.FC<WorkspaceProps> = () => {
+    const [paths, setPaths] = useState<any>([]);
+
+    const pathHandler = (path: any) => {
+        setPaths([...paths, path])
+    }
+
+    return (
+        <Grid container justify='space-between'>
+            <Grid item>
+                <Whiteboard paths={paths} addPath={pathHandler}/>
+            </Grid>
+            <Grid item>
+                <Whiteboard paths={paths} addPath={pathHandler}/>
+            </Grid> 
+        </Grid>
+    );
 }
 
 export default Workspace;
