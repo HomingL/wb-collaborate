aa027e63a176eb2419b9807207725525aa2965ad
Diego2941 <diego2941@hotmail.com>
2021-03-15 15:53:49 -0400

    Add WBcontext

--- frontend/src/components/whiteboard/wbContext.tsx
+++ frontend/src/components/whiteboard/wbContext.tsx
@@ -1,24 +1,29 @@
 
-import { useState, useContext, createContext } from 'react'
+import { useState, useContext, createContext, useEffect } from 'react'
 import { fabric } from "fabric";
 
 export interface WBprops {
-    penState: string;
-    setPenState: (state : string) => void;
+    penState: boolean;
+    setPenState: (state : boolean) => void;
     canvas: fabric.Canvas;
     setCanvas: (canv: fabric.Canvas) => void;
 }
 
 const WBContext = createContext<WBprops>({
-    penState: "",
-    setPenState: (state : string) => {},
+    penState: true,
+    setPenState: (state : boolean) => {},
     canvas: null,
-    setCanvas: (canv: fabric.Canvas) => void
+    setCanvas: (canv: fabric.Canvas) => {}
 });
 
 const WBProvider: React.FC = ({ children }) => {
     const [canvas, setCanvas] = useState<fabric.Canvas>();
-    const [penState, setPenState] = useState<string>("up");
+    const [penState, setPenState] = useState<boolean>(true);
+
+    useEffect( () =>{
+        if (canvas)
+            canvas.isDrawingMode = false;
+    }, [penState]);
 
     return (
         <WBContext.Provider value={{penState, setPenState, canvas, setCanvas}}>
