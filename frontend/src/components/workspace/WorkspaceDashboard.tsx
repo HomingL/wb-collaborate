8015f9b45636a4de56ab75ef464345eba86ef2a3
Diego2941 <diego2941@hotmail.com>
2021-04-03 19:21:13 -0400

    Add pagination

--- frontend/src/components/workspace/WorkspaceDashboard.tsx
+++ frontend/src/components/workspace/WorkspaceDashboard.tsx
@@ -5,15 +5,21 @@ import WhiteboardCard from './WhiteboardCard';
 import { useCreateWhiteboardMutation, useGetWhiteboardsLazyQuery, User } from '../../generated/apolloComponents';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Whiteboard from '../../../pages/whiteboard/[wid]';
+import Pagination from '@material-ui/lab/Pagination';
+
 
 // interface WorkspaceDashboardProps {
 //   whiteboards: Whiteboard[]
 // }
 
 const WorkspaceDashboard: React.FC = () => {
+    const pageLimit = 12;
     const classes = useStyles();
     const [wbName, setWbName] = useState<string>("");
     const [whiteboards, setWhiteboards] = useState<Whiteboard[]>([]);
+    const [allWhiteboards, setAllWhiteboards] = useState<Whiteboard[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [totalPage, settotalPage] = useState<number>(1);
 
     const [createWhiteboardMutation] = useCreateWhiteboardMutation({
       variables:{
@@ -42,9 +48,17 @@ const WorkspaceDashboard: React.FC = () => {
     }, []);
 
     useEffect(() =>{
-      if (data)
-        setWhiteboards(data?.GetWhiteboards);
+      if (data){
+        const pg = Math.ceil(data?.GetWhiteboards.length/pageLimit);
+        setAllWhiteboards(data?.GetWhiteboards);
+        settotalPage(pg ? pg : 1);
+      }
     }, [data, error]);
+
+    useEffect( () =>{
+      console.log("Page", page);
+      setWhiteboards(allWhiteboards.slice((page - 1)*pageLimit, (page)*pageLimit));
+    }, [allWhiteboards, page]);
 
     return (
       <Grid container spacing={3} justify='flex-start'>
@@ -61,11 +75,14 @@ const WorkspaceDashboard: React.FC = () => {
         </Grid>
 
         {whiteboards.map((whiteboard, index) => (
-          <Grid item xs={6} sm={2} key={index}>
+          <Grid item xs={3} key={index}>
             <WhiteboardCard {...whiteboard} />
           </Grid>
         ))}
 
+        <Grid className={classes.paging} container justify="center">        
+            <Pagination count={totalPage} color="primary" onChange={(_, pg) => setPage(pg)}/>
+        </Grid>
       </Grid>
     );
 }
@@ -90,9 +107,12 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: theme.spacing(3),
     },
     textArea:{
-        position: 'relative',
-        padding: theme.spacing(2),
-        margin: theme.spacing(1),
+      position: 'relative',
+      padding: theme.spacing(2),
+      margin: theme.spacing(1),
+    },
+    paging:{
+      margin: theme.spacing(10),
     }
   }));
 
