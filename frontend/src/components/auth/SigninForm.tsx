dd6e92315d90305af4693ab12b65bc0db53eb542
Hongming Li <homgming.li@mail.utoronto.ca>
2021-04-10 12:09:05 -0400

    Removed some unused in sign in form

--- frontend/src/components/auth/SigninForm.tsx
+++ frontend/src/components/auth/SigninForm.tsx
@@ -93,10 +93,10 @@ const SigninForm: React.FC<SigninFormProps> = () => {
           id="password"
           autoComplete="current-password"
         />
-        <FormControlLabel
+        {/* <FormControlLabel
           control={<Checkbox value="remember" color="primary" />}
           label="Remember me"
-        />
+        /> */}
         <Button
           type="submit"
           fullWidth
@@ -107,11 +107,11 @@ const SigninForm: React.FC<SigninFormProps> = () => {
           Sign In
         </Button>
         <Grid container>
-          <Grid item xs>
+          {/* <Grid item xs>
             <Link href="#" variant="body2">
               Forgot password?
             </Link>
-          </Grid>
+          </Grid> */}
           <Grid item>
             <Link href="/signup" variant="body2">
               {"Don't have an account? Sign Up"}
